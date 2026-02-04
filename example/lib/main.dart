6811120a030b70002ed88e9b7e2eedb1be2282c5
jaypanchal1014 <jaypanchal1014@gmail.com>
2026-02-04 15:22:04 +0530

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Custom Checkbox Example',
       home: Scaffold(
         appBar: AppBar(title: const Text('Custom Checkbox Example')),
